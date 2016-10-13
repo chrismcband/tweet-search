1bffc8aed009047e5f7b5123a64a5a0e11fee15e
sus111 <sgraham535@gmail.com>
2016-10-13 17:01:46 +0100

    refactor modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,30 +3,20 @@ import { connect } from 'react-redux';
 import ModalContent from './ModalContent';
 import './style/Modal.css';
 
-export class Modal extends Component {
-
-  render() {
-    const user = this.props.selectedUser;
-    if(!this.props.selectedUser) {
+function Modal(props) {
+  const user = props.selectedUser;
+  if(!props.selectedUser) {
       return <div></div>
-    }
-    else {
-      console.log(user);
-      return (
-        <div>
-          <div className="modal-overlay" onClick={() => this.props.hideModal()}>
-            <ModalContent user={user}/>
-          </div>
+  }
+  else {
+    return (
+      <div>
+        <div className="modal-overlay" onClick={() => props.hideModal()}>
+          <ModalContent user={user}/>
         </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    selectedUser: state.selectedUser
-  }
-}
-
-export default connect(mapStateToProps)(Modal);
+export default Modal;
